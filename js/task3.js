af8d3eb21b4d0fcb722847ecc6da6dc9955d68b7
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2023-10-05 21:57:43 +0300

    task

---
+++ js/task3.js
@@ -0,0 +1,16 @@
+// console.log('Hello!!!');
+// const arr = [1,2,3,4,5]
+// console.log(arr);
+
+// const emailInputName = "email";
+// const passwordInputName = "password";
+
+// const credentials = {
+//     [emailInputName]: "henry.carter@aptmail.com",
+//     [passwordInputName]: "jqueryismyjam",
+ 
+// };
+
+// console.log(emailInputName);
+
+
